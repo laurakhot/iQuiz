cc72f732f0307f47ab5c31c1a9a67072b36947f0
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-05-04 15:54:26 -0700

    Initial Commit

---
+++ iQuizTests/iQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizTests.swift
+//  iQuizTests
+//
+//  Created by Laura Khotemlyansky on 5/4/25.
+//
+
+import Testing
+@testable import iQuiz
+
+struct iQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
